c1784bdd75b8d507b0f6a3f47f50b3ec819911b3
Caspar Skripkauskas <caspar@completewithus.com>
2020-05-19 08:42:04 +0300

    update

--- webpack/tailwind.config.js
+++ webpack/tailwind.config.js
@@ -1,9 +1,11 @@
 let colors = {
   black: '#131313',
   red: '#ff3700',
+  lipstick: '#dd143c',
   white: '#fff',
   grey: '#676767',
-  transparent: 'transparent'
+  transparent: 'transparent',
+  pink: '#e86680'
 }
 
 module.exports = {
@@ -11,10 +13,27 @@ module.exports = {
   theme: {
     extend: {
       colors,
+      spacing: {
+        '1px': '1px',
+        '2px': '2px',
+        '18': '4.5rem',
+        '36': '9rem',
+        '38': '9.5rem',
+        '78': '19.5rem'
+      },
       inset: {
         '4': '1rem',
         '8': '2rem',
-        '16': '4rem'
+        '16': '4rem',
+        '24': '6rem',
+        '1/2': '50%'
+      },
+      fontFamily: {
+        'primary': 'Akzidenz-Grotesk, sans-serif',
+        'secondary': 'Lubalin Graph, sans-serif'
+      },
+      opacity: {
+        '35': '0.35'
       }
     },
   },
